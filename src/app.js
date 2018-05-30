77b689a8b0e629c16fd2c02977355cbde5cc91a1
dori claudino <dori.claudino@gmail.com>
2018-05-30 17:41:33 -0400

    TodoList and Todo components in separeted files.

--- src/app.js
+++ src/app.js
@@ -6,148 +6,14 @@ import { createStore } from 'redux'
 import { connect } from 'react-redux'
 import AddTodo from './components/AddTodo';
 import Footer from './components/Footer'
-
-/******
-todo reducer
-/******/
-const todo = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      };
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-
-      return {
-        ...state,
-        completed: !state.completed
-      };
-    default:
-      return state;
-  }
-};
-
-
-/******
-todos reducer
-/******/
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
-    case 'EMPTY_TODOS':
-      return [];
-    default:
-      return state;
-  }
-};
-
-const toggleTodo = (id) => {
-  return {
-    type: 'TOGGLE_TODO',
-    id
-  }
-}
-
-/******
-filter reducer
-/******/
-const getVisibleTodos = (
-  todos,
-  filter
-) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_ACTIVE':
-      return todos.filter(t => !t.completed);
-    case 'SHOW_COMPLETED':
-      return todos.filter(t => t.completed);
-    default:
-      return todos || [];
-  }
-};
-
-
-/******
-visibility reducer
-return the default SHOW_ALL using return state
-/******/
-const visibilityFilter = (state = 'SHOW_ALL', action) => {
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter;
-    default:
-      return state;
-  }
-}
+import VisibleTodoList from './components/VisibleTodoList'
+import visibilityFilter from './reducers/visibilityFilter'
+import todos from './reducers/todos'
 
 const todoApp = combineReducers({
   todos,
   visibilityFilter
 })
-
-const Todo = ({
-  onClick,
-  completed,
-  text
-}) => (
-    <li
-      onClick={onClick}
-      style={{
-        textDecoration:
-        completed ?
-          'line-through' :
-          'none'
-      }}
-    >
-      {text}
-    </li>
-  );
-
-const TodoList = ({
-  todos,
-  onTodoClick
-}) => (
-    <ul>
-      {todos.map(todo =>
-        <Todo
-          key={todo.id}
-          {...todo}
-          onClick={() => onTodoClick(todo.id)}
-        />
-      )}
-    </ul>
-  );
-
-const mapVisibleTodoListStateToProps = (state) => {
-  return {
-    todos : getVisibleTodos(state.todos,
-      state.visibilityFilter)
-  }
-}
-const mapVisibleTodoListDispatchToProps = (dispatch) => {
-  return {
-    onTodoClick : (id) => {
-      dispatch(toggleTodo(id))
-    }
-  }
-}
-const VisibleTodoList = connect(
-  mapVisibleTodoListStateToProps,
-  mapVisibleTodoListDispatchToProps
-)(TodoList);
-
 
 class Provider extends Component {
   getChildContext() {
@@ -175,14 +41,14 @@ const TodoApp = () =>
     <Footer />
   </div>
   )
-  
+
 
 const App = () => {
-    return (
-        <Provider store={createStore(todoApp)}>
-            <TodoApp />
-        </Provider>
-    );
+  return (
+    <Provider store={createStore(todoApp)}>
+      <TodoApp />
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Todo = ({
+    onClick,
+    completed,
+    text
+}) => (
+        <li
+            onClick={onClick}
+            style={{
+                textDecoration:
+                    completed ?
+                        'line-through' :
+                        'none'
+            }}
+        >
+            {text}
+        </li>
+    );
+    
+export default Todo;
