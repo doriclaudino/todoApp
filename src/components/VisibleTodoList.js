dfef116bc0bd549fc2766c8394212167db261e0a
dori claudino <dori.claudino@gmail.com>
2018-05-31 11:58:09 -0400

    selectors with reducers

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -2,19 +2,7 @@ import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from './TodoList'
 import { withRouter } from 'react-router-dom'
-
-const getVisibleTodos = (todos, filter) => {
-    switch (filter) {
-        case 'all':
-            return todos;
-        case 'completed':
-            return todos.filter(t => t.completed);
-        case 'active':
-            return todos.filter(t => !t.completed);
-        default:
-            throw new Error(`Unknown filter: ${filter}.`);
-    }
-};
+import { getVisibleTodos } from '../reducers'
 
 const mapVisibleTodoListStateToProps = (state, withRouterParams) => ({
     todos: getVisibleTodos(
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -37,3 +37,16 @@ const todos = (state = [], action) => {
 };
 
 export default todos;
+
+export const getVisibleTodos = (todos, filter) => {
+    switch (filter) {
+        case 'all':
+            return todos;
+        case 'completed':
+            return todos.filter(t => t.completed);
+        case 'active':
+            return todos.filter(t => !t.completed);
+        default:
+            throw new Error(`Unknown filter: ${filter}.`);
+    }
+};
